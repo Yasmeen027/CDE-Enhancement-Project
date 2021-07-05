5d7e4e3d8f8504840f1361257568427a05885a84
Yasmeen027 <syedyasmeen027@gmail.com>
2021-07-06 00:12:23 +0530

    Collateral Loans Risk Assessment

---
+++ Collateral Loans Risk Assessment/authorization-service/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into user(id, name, password) values (1, 'naima', 'naima');
+
+
+
+
+
+insert into customer(id,name,password) values (100011,'yasmeen ','yasmeen');
+insert into customer(id,name,password) values (100012,'asifa,'asifa');
